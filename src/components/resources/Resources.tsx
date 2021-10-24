4d176227b452c2d1b199e7efb5d8bfacf3460cd9
ha08ya.a <ha08ya@gmail.com>
2021-10-25 05:44:40 +0900

    [Add] Resource

---
+++ src/components/resources/Resources.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type TResource = {
+  id: number;
+  title: string;
+};
+
+type TResources = {
+  resources: TResource[];
+};
+
+const Resources = (props: TResources) => {
+  return (
+    <>
+      {props.resources.map((resource) => {
+        return <p key={resource.id}>{resource.title}</p>;
+      })}
+    </>
+  );
+};
+
+export default Resources;
