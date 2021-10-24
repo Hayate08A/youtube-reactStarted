54fd1d32885a7850ca0dd9f28f51434c3c11019c
ha08ya.a <ha08ya@gmail.com>
2021-10-25 06:03:15 +0900

    [Add] #7 axios を用いたHTTP通信の実装
    as [] でいいのか・・・？

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Counter from '../counter/Counter';
+import Button from '../button/Button';
+import jsonplaceholder from '../../apis/jsonplaceholder';
+import Resources from '../resources/Resources';
 
 export const App = () => {
+  const [resources, setResources] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const posts = await jsonplaceholder.get('/post');
+      setResources(posts.data as []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getAlbums = async () => {
+    try {
+      const albums = await jsonplaceholder.get('/albums');
+      setResources(albums.data as []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='ui container app_container'>
+      <Button onClick={getPosts} buttonColor='primary' title='posts' />
+      <Button onClick={getAlbums} buttonColor='red' title='albums' />
+      <Resources resources={resources} />
       <Counter />
     </div>
   );
