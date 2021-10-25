a60fc645880e436d12e4ac20fce24b1d6c379a38
ha08ya.a <ha08ya@gmail.com>
2021-10-26 03:16:32 +0900

    [Add] onSubmit
    eventの型は・・・？

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const SearchBar = () => {
+  const onFormSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    console.log('onSubmit');
+  };
   return (
     <div className='search_bar_container'>
-      <form className='ui form'>
+      <form className='ui form' onSubmit={onFormSubmit}>
         <div className='field'>
-          <label>First Name</label>
-          <input type='text' name='first-name' placeholder='First Name' />
+          <label>Image Search</label>
+          <input type='text' name='search' placeholder='' />
         </div>
       </form>
     </div>
