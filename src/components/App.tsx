f1ba8eb9cef17e9a3f6f0233b3524aa5c2562d06
ha08ya.a <ha08ya@gmail.com>
2021-10-26 02:41:53 +0900

    [Fix] import error

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Counter from './counter/Counter';
+
+export const App = () => {
+  return (
+    <div className='ui app-container'>
+      <Counter />
+    </div>
+  );
+};
