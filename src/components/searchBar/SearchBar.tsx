1bb1c987ec19e754f0001e58dced2477527731fb
ha08ya.a <ha08ya@gmail.com>
2021-10-26 03:10:43 +0900

    [WIP] marginの相殺()

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const SearchBar = () => {
   return (
-    <form className='ui form'>
-      <div className='field'>
-        <label>First Name</label>
-        <input type='text' name='first-name' placeholder='First Name' />
-      </div>
-    </form>
+    <div className='search_bar_container'>
+      <form className='ui form'>
+        <div className='field'>
+          <label>First Name</label>
+          <input type='text' name='first-name' placeholder='First Name' />
+        </div>
+      </form>
+    </div>
   );
 };
 
