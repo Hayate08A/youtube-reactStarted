4b893e498c0ad7499dfb1feb2ba40d7a441a08f1
ha08ya.a <ha08ya@gmail.com>
2021-10-27 03:03:08 +0900

    [Add] logo pixabay

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -7,6 +7,7 @@ import jsonplaceholder from '../../apis/jsonplaceholder';
 import Resources from '../resources/Resources';
 import SearchBar from '../searchBar/SearchBar';
 import ImageList from '../imageList/ImageList';
+import pixabayLogo from '../../logo.png';
 
 export const App = () => {
   const [resources, setResources] = useState([]);
@@ -50,6 +51,7 @@ export const App = () => {
 
   return (
     <div className='ui container app_container'>
+      <img src={pixabayLogo} alt='pixabay-logo' />
       <SearchBar onSubmit={onSearchSubmit} />
       <ImageList images={images} />
       <></>
